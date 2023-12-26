ef0f116de598542f49e4e457098f9de6c90a835d
MostafaGamal7 <drshmostafa62@gmail.com>
2023-12-26 13:24:17 +0200

    added cors

--- index.js
+++ index.js
@@ -3,11 +3,15 @@ const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 const rangeMiddleware = require("./range"); // Import your range.js file
+const cors = require("cors"); // Import the CORS middleware
 
 const port = process.env.PORT || 8080;
 
 // Use your custom middleware to handle Content-Range header
 server.use(rangeMiddleware);
+
+// Enable CORS for all routes
+server.use(cors());
 
 server.use(middlewares);
 server.use(router);
